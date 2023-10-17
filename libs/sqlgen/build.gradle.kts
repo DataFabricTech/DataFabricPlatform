bf896d435a654f203c3a49619c22b43be81fa60a
seungfwani <seungfwani2@gmail.com>
2023-10-17 17:21:02 +0900

    add sqlgen for select, where

--- libs/sqlgen/build.gradle.kts
+++ libs/sqlgen/build.gradle.kts
@@ -3,3 +3,7 @@ group = "com.mobigen.libs"
 plugins {
     id("com.mobigen.java-library")
 }
+
+dependencies {
+    implementation("org.mybatis.dynamic-sql:mybatis-dynamic-sql:1.5.0")
+}
--- libs/sqlgen/src/main/java/com/mobigen/sqlgen/list/SqlBuilder.java
+++
@@ -1,9 +0,0 @@
-package com.mobigen.sqlgen.list;
-
-public class SqlBuilder {
-
-    public static SqlTable from(String name) {
-        return SqlTable.of(name);
-    }
-
-}
