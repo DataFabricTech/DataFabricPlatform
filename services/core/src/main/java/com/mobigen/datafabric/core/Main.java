0af7e505f6366983e27ddf3457bb0bdafc9889c7
seungfwani <seungfwani2@gmail.com>
2023-10-23 11:36:40 +0900

    delete unused

--- services/core/src/main/java/com/mobigen/datafabric/core/Main.java
+++ services/core/src/main/java/com/mobigen/datafabric/core/Main.java
@@ -9,17 +9,21 @@ import lombok.extern.slf4j.Slf4j;
 import java.io.IOException;
 
 @Slf4j
-public class Main{
+public class Main {
     public static void main(String[] args) {
-        log.error( "Hello World!");
-        GRPCServer server = new GRPCServer( 9360, 10 );
-        StorageServiceCallBack cb = new StorageServiceImpl();
-        StorageService service = new StorageService( cb );
+        log.error("Hello World!");
+        GRPCServer server = new GRPCServer(9360, 10);
         try {
-            server.addService( service );
+            initService(server);
             server.start();
-        } catch( IOException | InterruptedException e ) {
-            throw new RuntimeException( e );
+        } catch (IOException | InterruptedException e) {
+            throw new RuntimeException(e);
         }
+    }
+
+    private static void initService(GRPCServer server) throws IOException {
+        StorageServiceCallBack cb = new StorageServiceImpl();
+        StorageService service = new StorageService(cb);
+        server.addService(service);
     }
 }
