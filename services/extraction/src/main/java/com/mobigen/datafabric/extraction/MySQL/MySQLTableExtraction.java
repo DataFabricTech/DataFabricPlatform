754e9824e9012ad4edcb76ececbf3532a084a676
eunmiGo <ekf0412@mobigen.com>
2024-01-15 14:50:05 +0900

    [Fabric] RDBMS 클래스 통합
    - Postgres, MariaDB, MySQL 공통 추출 클래스 작성

--- services/extraction/src/main/java/com/mobigen/datafabric/extraction/MySQL/MySQLTableExtraction.java
+++ services/extraction/src/main/java/com/mobigen/datafabric/extraction/MySQL/MySQLTableExtraction.java
@@ -1,15 +1,7 @@
 package com.mobigen.datafabric.extraction.MySQL;
 
 import com.mobigen.datafabric.extraction.RDBDefault.RDBDefaultTableExtraction;
-import com.mobigen.datafabric.extraction.extraction.Extract;
 import com.mobigen.datafabric.extraction.model.TargetConfig;
-
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.HashMap;
-import java.util.Objects;
 
 public class MySQLTableExtraction extends RDBDefaultTableExtraction {
     public MySQLTableExtraction(TargetConfig target) {
