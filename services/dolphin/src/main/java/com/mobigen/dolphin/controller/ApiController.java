8aab27f46841874011db6cfd68bb9841ab6ec3e8
seungfwani <seungfwani2@gmail.com>
2024-05-31 17:27:56 +0900

    [FAB2023-778] Update swagger

--- services/dolphin/src/main/java/com/mobigen/dolphin/controller/ApiController.java
+++ services/dolphin/src/main/java/com/mobigen/dolphin/controller/ApiController.java
@@ -41,26 +41,31 @@ public class ApiController {
         return modelService.createModel(createModelDto);
     }
 
+    @Operation(summary = "Execute Query using DataModel")
     @PostMapping("/query/execute")
     public QueryResultDTO execute(@RequestBody ExecuteDto executeDto) {
         return queryService.execute(executeDto.getQuery());
     }
 
+    @Operation(summary = "Async Execute Query using DataModel")
     @PostMapping("/query/async/execute")
     public Object asyncExecute(@RequestBody ExecuteDto executeDto) {
         return queryService.executeAsync(executeDto.getQuery());
     }
 
+    @Operation(summary = "Read result data of asynchronous query using JobId")
     @GetMapping("/query/read/{job_id:^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$}")
     public Object read(@PathVariable("job_id") UUID jobId) {
         return queryService.read(jobId);
     }
 
+    @Operation(summary = "Download result data of asynchronous query using JobId")
     @GetMapping("/query/download/{job_id:^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$}")
     public Object download(@PathVariable("job_id") UUID jobId) {
         return null;
     }
 
+    @Operation(summary = "Check status of asynchronous query job using JobId")
     @GetMapping("/query/status/{job_id:^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$}")
     public Object status(@PathVariable("job_id") UUID jobId) {
         return queryService.status(jobId);
