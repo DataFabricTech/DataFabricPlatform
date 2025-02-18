055d801a4f284831b311357435785555df67104a
seungeon <djs110615@mju.ac.kr>
2025-02-18 11:38:26 +0900

    Feat: swagger-ui 설정

---
+++ services/monitoring/src/main/java/com/mobigen/monitoring/config/SwaggerConfig.java
@@ -0,0 +1,29 @@
+package com.mobigen.monitoring.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import lombok.RequiredArgsConstructor;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@RequiredArgsConstructor
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("My API Documentation")
+                        .version("1.0")
+                        .description("API documentation using springdoc-openapi and Swagger UI"));
+    }
+
+    @Bean
+    public GroupedOpenApi publicApi() {
+        return GroupedOpenApi.builder()
+                .group("v1")
+                .pathsToMatch("/api/v1/**") // 특정 경로만 API 문서에 포함
+                .build();
+    }
+}
