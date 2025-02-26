953c5735ae983afec0c826a125caa7c7d8d0e2cb
임준범 <jblim0125@gmail.com>
2025-02-26 19:35:59 +0900

    JPA 옵션 설정, flyway 업데이트, 로그 출력 포맷 변경

--- services/vdap-server/src/main/resources/db/migration/V1__init.sql
+++ services/vdap-server/src/main/resources/db/migration/V1__init.sql
@@ -14,32 +14,66 @@ create table storage_service_entity (
 create index storage_service_entity_name_index on storage_service_entity (name);
 create index index_storage_service_entity_deleted on storage_service_entity (id, deleted);
 
+
 create table classification
 (
-    id        varchar(36)   primary key,
-    name      varchar(256),
-    json      json          not     null,
-    updatedAt datetime(3),
-    updatedBy varchar(256),
-    deleted   tinyint(1),
-    constraint unique_name
-        unique (name)
+    id         varchar(36)   primary key,
+    name       varchar(256),
+    json       json          not     null,
+    updated_at datetime(3),
+    updated_by varchar(256)
 );
 
 create index classification_entity_name_index on classification (name);
-create index index_classification_deleted on classification (`id`, deleted);
-
 
 create table tag (
     `id`              varchar(36)   primary key,
-    classification_id varchar(36)   not null,
+    classification_id varchar(36)   not     null,
     name              varchar(256),
-    json              json          not null,
-    updatedAt         datetime(3),
-    updatedBy         varchar(256),
-    deleted           tinyint(1),
-    constraint unique_name
-        unique (classification_id, name)
+    json              json          not     null,
+    updated_at        datetime(3),
+    updated_by        varchar(256)
 );
 
-create index index_tag_deleted on tag (`id`, deleted);
+create index tag_entity_name_index on tag (classification_id, name);
+
+create table tag_usage (
+    source      tinyint      not null,
+    tag_id      varchar(256) not null,
+    label_type  tinyint      not null,
+    state       tinyint      not null,
+    target_type varchar(32)  not null,
+    target_id   varchar(36)  not null,
+    constraint tag_usage_key
+        unique (source, tag_id, target_type, target_id)
+);
+
+create table entity_relationship
+(
+    from_id     varchar(36)          not null,
+    to_id       varchar(36)          not null,
+    from_entity varchar(256)         not null,
+    to_entity   varchar(256)         not null,
+    relation   tinyint              not null,
+    json_schema varchar(256)         null,
+    json       json                 null,
+    deleted    tinyint(1) default 0 not null,
+    primary key (from_id, to_id, relation)
+);
+
+create index from_entity_type_index on entity_relationship (from_id, from_entity);
+
+create index from_index on entity_relationship (from_id, relation);
+
+create index idx_entity_relationship_fromEntity_fromId_relation
+    on entity_relationship (from_entity, from_id, relation);
+
+create index idx_er_fromEntity_fromId_toEntity_relation
+    on entity_relationship (from_entity, from_id, to_entity, relation);
+
+create index to_entity_type_index on entity_relationship (to_id, to_entity);
+
+create index to_index on entity_relationship (to_id, relation);
+
+
+
