e2d6c1464bcbf05ccb3940cd5685252a8cec5a5a
seungeon <djs110615@mju.ac.kr>
2025-04-30 09:42:47 +0900

    Fix: mysql query 문법 에러 해결

--- services/monitoring/src/main/resources/sql/mysql/get-slow-query.sql
+++ services/monitoring/src/main/resources/sql/mysql/get-slow-query.sql
@@ -1,7 +1,7 @@
 SELECT
     CAST(sql_text AS CHAR) AS sql_text,
     COUNT(*) AS total_count,
-    ROUND(AVG(query_time), 1) AS avg_exec_time,
+    ROUND(AVG(query_time), 1) AS avg_exec_time
 FROM
     mysql.slow_log
 GROUP BY
